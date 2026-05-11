0eb2375a434c3b6abe213fa38caaff5853ddc614
chjohnson80505-tech <chjohnson80505@icloud.com>
2026-05-11 01:18:28 -0700

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// config.js – keep this file in GitHub
-const backend = "https://og-clan-bots-backend.onrender.com/";
-const stripePub = "pk_test_XXXXXXXXXXXXXXXXXXXXXXXX"; // <‑ replace with your Stripe publishable key
