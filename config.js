9dfb96c2908852474a567971a2f9c0d61f4a0077
chjohnson80505-tech <chjohnson80505@icloud.com>
2026-05-11 01:13:49 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js – keep this file in GitHub
+const backend = "https://og-clan-bots-backend.onrender.com/";
+const stripePub = "pk_test_XXXXXXXXXXXXXXXXXXXXXXXX"; // <‑ replace with your Stripe publishable key
